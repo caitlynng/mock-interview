22341db3e2163340f7b87eaba5222aa665e86e6d
catbouce <75095386+catbouce@users.noreply.github.com>
2023-07-05 21:35:41 -0700

    refactor(style): update <main> layout to max width of 1500px

--- src/components/question-panel/QuestionList.styles.ts
+++ src/components/question-panel/QuestionList.styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  width: 95vw;
   margin: 0 auto;
+  padding: 2em;
 `;
--- src/pages/Interview.styles.ts
+++ src/pages/Interview.styles.ts
@@ -3,4 +3,5 @@ import styled from 'styled-components';
 export const InterviewWrapper = styled.main`
   width: 100%;
   height: 100%;
+  max-width: 1500px;
 `;
