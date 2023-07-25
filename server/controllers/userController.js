9056bb442852d92540a7755cceb61c512e0a5f8b
catbouce <75095386+catbouce@users.noreply.github.com>
2023-07-24 21:46:25 -0700

    feat(API): add API endpoint get-question to generate question from OpenAI

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
 import { StatusCodes } from "http-status-codes";
 import Question from "../models/Question.js";
+import "dotenv/config";
 
 export const getAllQuestions = async (req, res) => {
   const { userId } = req.user;
@@ -35,4 +35,43 @@ export const deleteQuestion = async (req, res) => {
   const update = await Question.findOneAndDelete(questionID);
 
   res.status(StatusCodes.OK).json({ update });
+};
+export const getQuestion = async (req, res) => {
+  console.log("get question");
+  const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+  const url = "https://api.openai.com/v1/chat/completions";
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${OPENAI_API_KEY}`,
+  };
+
+  const data = {
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: "user", content: "Generate an interview behavioral question" },
+    ],
+  };
+
+  async function callOpenAI() {
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      res.status(StatusCodes.OK).json({ result });
+    } catch (error) {
+      // Handle any errors that occurred during the fetch request
+      console.error("Error:", error);
+    }
+  }
+
+  callOpenAI();
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,11 +6,13 @@ import {
   addQuestion,
   editQuestion,
   deleteQuestion,
+  getQuestion,
 } from "../controllers/userController.js";
 
 router.route("/all-questions").get(getAllQuestions);
 router.route("/add-question").post(addQuestion);
 router.route("/edit-question").post(editQuestion);
 router.route("/delete-question").post(deleteQuestion);
+router.route("/get-question").get(getQuestion);
 
 export default router;
